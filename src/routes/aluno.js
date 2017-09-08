6911f074f15b7850b4e7ed1426bcb0cdaadcb76b
André Dalcin <andre@planosassesoria.com.br>
2017-09-08 12:08:12 -0300

    removed finally keys

--- src/routes/aluno.js
+++ src/routes/aluno.js
@@ -7,36 +7,36 @@ const aluno = deps => {
         res.json(await db.aluno().all())
       } catch (err) {
         res.status(500).send(err)
-      } finally {
-        next()
       }
+
+      next()
     })
     .post('/aluno', async (req, res, next) => {
       try {
         res.status(201).json(await db.aluno().save(req.body))
       } catch (err) {
         res.status(500).send(err)
-      } finally {
-        next()
       }
+
+      next()
     })
     .put('/aluno', async (req, res, next) => {
       try {
         res.status(204).json(await db.aluno().update(req.body))
       } catch (err) {
         res.status(500).send(err)
-      } finally {
-        next()
       }
+
+      next()
     })
     .delete('/aluno/:id', async (req, res, next) => {
       try {
         res.status(204).json(await db.aluno().delete(req.params.id))
       } catch (err) {
         res.status(500).send(err)
-      } finally {
-        next()
       }
+
+      next()
     })
 }
 
